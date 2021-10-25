7bbae15712cd0c8c3a60867779243e16423783bc
KaErk <1411909194@qq.com>
2021-10-26 00:46:03 +0800

    加入显示天气的功能

--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
@@ -8,11 +8,17 @@ import com.sunnyweather.android.logic.model.Place
 
 class PlaceViewModel : ViewModel() {
     private val searchLiveData = MutableLiveData<String>()
+
     val placeList = ArrayList<Place>()
+
     val placeLiveData = Transformations.switchMap(searchLiveData) { query ->
         Repository.searchPlaces(query)
     }
     fun searchPlaces(query: String) {
         searchLiveData.value = query
     }
+
+    fun savePlace(place: Place) = Repository.savePlace(place)
+    fun getSavedPlace() = Repository.getSavedPlace()
+    fun isPlaceSaved() = Repository.isPlaceSaved()
 }
